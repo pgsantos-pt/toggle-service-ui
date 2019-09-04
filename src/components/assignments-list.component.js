51deb2bc8ab1dab6ac20e50b605f0136c4dca804
pgsantos-pt <santos.pedro.gil@gmail.com>
2019-09-04 20:06:37 +0100

    Introduction of assignments

--- src/components/assignments-list.component.js
+++ src/components/assignments-list.component.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const Assignment = props => (
-    <tr>
-        <td>{props.assignment.toggleAssignments[0].toggleOwner}</td>
-        <td>{props.assignment.toggleAssignments[0].toggleValue}</td>
-    </tr>
-)
+const Assignment = props => {
+    return (
+      <tr>
+          <td>{props.assignment.toggleOwner}</td>
+          <td>{props.assignment.toggleValue.toString()}</td>
+      </tr>
+    )
+}
+
+const Toggle = props => {
+    let assignments;
+
+    if(props.toggle.toggleAssignments.length>0) {
+        assignments = props.toggle.toggleAssignments.map(
+          function(currentAssignment, i){
+              return <Assignment assignment={currentAssignment} key={i} />;
+      });
+    }
+
+    return (
+        <div>
+            <label>Toggle name: {props.toggle.toggleName}</label>
+            <div>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
+                    <thead>
+                        <tr>
+                            <th width={"85%"}>Toggle Owner</th>
+                            <th>Toggle Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {assignments}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+      )
+}
 
 export default class AssignmentsList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            assignments: []
+            toggle: []
         };
     }
 
     componentDidMount() {
         axios.get('http://localhost:8080/toggles/'+this.props.match.params.id+'/toggle-assignments')
             .then(response => {
-                this.setState({ assignments: response.data });
-                console.log(this.state.assignments[0].toggleAssignments[0].toggleOwner);
+                this.setState({ toggle: response.data });
+                console.log(this.state.toggle);
             })
             .catch(function (error){
                 console.log(error);
@@ -29,27 +61,21 @@ export default class AssignmentsList extends Component {
     }
 
     toggleList() {
-        return this.state.assignments.map(
+        if(this.state.toggle.length===0) {
+            return <div><label>There are no assignments yet</label></div>
+        }
+
+        return this.state.toggle.map(
             function(currentToggle, i){
-                return <Assignment assignment={currentToggle} key={i} />;
+                return <Toggle toggle={currentToggle} key={i} />;
         })
     }
 
     render() {
         return (
             <div>
-                <h3>Toggle Assignment List</h3>
-                <table className="table table-striped" style={{ marginTop: 20 }} >
-                    <thead>
-                        <tr>
-                            <th>Toggle ID</th>
-                            <th>Toggle Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { this.toggleList() }
-                    </tbody>
-                </table>
+                <h3>Toggle Assignments List</h3>
+                {this.toggleList()}
             </div>
         )
     }
